e804188125958ec2abba291d82c4cef8d689df71
Layth Zahid <laith43d@gmail.com>
2021-09-28 00:56:42 +0300

    optimal solution is a key ;)

--- python-pass.py
+++ python-pass.py
@@ -26,22 +26,4 @@ class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        # Create a string to store our resultant palindrome
-        palindrome = ''
-
-        # loop through the input string
-        for i in range(len(s)):
-
-            # loop backwards through the input string
-            for j in range(len(s), i, -1):
-
-                # Break if out of range
-                if len(palindrome) >= j - i:
-                    break
-
-                # Update variable if matches
-                elif s[i:j] == s[i:j][::-1]:
-                    palindrome = s[i:j]
-                    break
-
-        return palindrome
+        pass
