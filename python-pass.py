b3bd604a94019a41bb6c25d0ce9839457d136b46
Layth Zahid <laith43d@gmail.com>
2021-09-28 00:56:09 +0300

    found the solution

--- python-pass.py
+++ python-pass.py
@@ -1,8 +1,47 @@
-# Given a string s, return whether s is a palindrome string.
+# Given a string s, return the longest palindromic substring in s.
+#
+# Example 1:
+#
+# Input: s = "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+#
+# Example 2:
+#
+# Input: s = "cbbd"
+# Output: "bb"
+#
+# Example 3:
+#
+# Input: s = "a"
+# Output: "a"
+#
+# Example 4:
+#
+# Input: s = "ac"
+# Output: "a"
 
 
 class Solution:
 
     @staticmethod
-    def palindrome(s: str) -> bool:
-        pass
+    def longest_palindromic(s: str) -> str:
+        # Create a string to store our resultant palindrome
+        palindrome = ''
+
+        # loop through the input string
+        for i in range(len(s)):
+
+            # loop backwards through the input string
+            for j in range(len(s), i, -1):
+
+                # Break if out of range
+                if len(palindrome) >= j - i:
+                    break
+
+                # Update variable if matches
+                elif s[i:j] == s[i:j][::-1]:
+                    palindrome = s[i:j]
+                    break
+
+        return palindrome
