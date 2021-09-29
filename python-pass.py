fbb2af09d45eddd2df6e96cd988e87b162739672
s4tt4r <91577493+s4tt4r@users.noreply.github.com>
2021-09-29 06:33:14 +0300

    the_answer.py
    it's not my code i wasn't know anything about the subject but i tried to understand whatever i can 
    *my main issue in the code is how to get an input either from the user or initial value

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,20 @@
-
-
-class Solution:
-
+class Soulution:
     @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+    def longestpalindrome (self, s:str) ->str:
+        
+        def holder(l,r):
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1 : r]
+        result = ""
+        for i in range(len(s)):
+            test = holder(i,i)
+            if len(test) > len (result): result = test
+            
+        return result
+    def results(self, s:str) :
+        
+        answer = Soulution().longestpalindrome()
+        print (answer)
+        results("babad")
